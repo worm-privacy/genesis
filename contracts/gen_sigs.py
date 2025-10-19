ab71554b7ce255df2ee3dca1bdd132f7fec29982
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-10-19 11:56:11 +0330

    Also include cliff/vesting in contract

--- contracts/gen_sigs.py
+++ contracts/gen_sigs.py
@@ -1,13 +1,13 @@
 # 1. Generate a private/public key (Report public key)
-# 2. Read from a CSV file (Address/Amount)
-# 3. Sign (Address/Amount) pair
-# 4. Export (Address/Amount/Signature) in a file
+# 2. Read from a CSV file (Address/Amount/Cliff/Vesting)
+# 3. Sign (Address/Amount/Cliff/Vesting) pair
+# 4. Export (Address/Amount/Cliff/Vesting/Signature) in a file
 
 # Contract:
 # Constructor(pubKey)
 #   Store pubkey
 # 
-# getCoins(address/amount/signature)
+# getCoins(address/amount/cliff/vesting/signature)
 #   Verify signature is valid for address amount (With stored pub key)
 #   Disallow same address mintin twice
 #   If everything ok: transfer WORM to address
