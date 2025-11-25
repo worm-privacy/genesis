204c280453f03cdcae2310dd1e80d5b708601eb0
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-11-25 14:07:13 +0330

    test: Add some tests

--- contracts/test/Genesis.t.sol
+++ contracts/test/Genesis.t.sol
@@ -1,24 +1,159 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {Genesis} from "../src/Genesis.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FakeERC20 is ERC20 {
-    constructor() ERC20("Fake Token", "FAKE") {
+contract MockToken is ERC20 {
+    constructor() ERC20("MockToken", "MTK") {
         _mint(msg.sender, 1_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
     }
 }
 
 contract GenesisTest is Test {
-    Genesis public genesis;
-    FakeERC20 public token;
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
 
-    address master = address(0xa11ce);
+    Genesis genesis;
+    MockToken token;
+
+    uint256 masterKey = 0xABCD;
+    address master = vm.addr(0xABCD);
+    address user = address(0xBEEF);
 
     function setUp() public {
-        token = new FakeERC20();
-        genesis = new Genesis(master, token);
+        vm.warp(123456);
+
+        token = new MockToken();
+        genesis = new Genesis(master, IERC20(token));
+
+        // Fund contract with tokens
+        token.mint(address(genesis), 1_000_000 ether);
+    }
+
+    function _signShare(Genesis.Share memory share, uint256 privKey) internal returns (bytes memory) {
+        bytes memory abiShare = abi.encode(share);
+        bytes32 hash = keccak256(abiShare).toEthSignedMessageHash();
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, hash);
+        return abi.encodePacked(r, s, v);
+    }
+
+    function testRevealAndTriggerSharpEmission() public {
+        // Create a share with a sharp emission
+        Genesis.SharpEmission[] memory sharp = new Genesis.SharpEmission[](1);
+        sharp[0] = Genesis.SharpEmission(block.timestamp - 10, 100 ether);
+
+        Genesis.LinearEmission memory linear = Genesis.LinearEmission(0, 0, 0);
+
+        Genesis.Share memory share =
+            Genesis.Share({id: 1, owner: user, sharpEmissions: sharp, linearEmission: linear, totalCap: 100 ether});
+
+        // Use a private key corresponding to master for testing
+
+        bytes memory sig = _signShare(share, masterKey);
+
+        // Cheat to set master private key
+        vm.prank(master);
+        genesis.reveal(share, sig);
+
+        // Trigger claim
+        vm.prank(user);
+        genesis.trigger(1);
+
+        // Check balance
+        assertEq(token.balanceOf(user), 100 ether);
+        assertEq(genesis.shareClaimed(1), 100 ether);
+    }
+
+    function testLinearEmission() public {
+        // Linear emission of 1 ether per second, cap 10 ether
+        Genesis.LinearEmission memory linear = Genesis.LinearEmission(block.timestamp - 5, 1 ether, 10 ether);
+        Genesis.SharpEmission[] memory sharp;
+
+        Genesis.Share memory share =
+            Genesis.Share({id: 2, owner: user, sharpEmissions: sharp, linearEmission: linear, totalCap: 10 ether});
+
+        bytes memory sig = _signShare(share, masterKey);
+
+        vm.prank(master);
+        genesis.reveal(share, sig);
+
+        vm.prank(user);
+        genesis.trigger(2);
+        assertEq(token.balanceOf(user), 5 ether);
+
+        // Trigger after 5 seconds
+        vm.warp(block.timestamp + 5);
+        vm.prank(user);
+        genesis.trigger(2);
+        assertEq(token.balanceOf(user), 10 ether);
+    }
+
+    function testClaimCannotExceedTotal() public {
+        Genesis.SharpEmission[] memory sharp = new Genesis.SharpEmission[](1);
+        sharp[0] = Genesis.SharpEmission(block.timestamp - 1, 100 ether);
+
+        Genesis.LinearEmission memory linear = Genesis.LinearEmission(block.timestamp - 1, 50 ether, 50 ether);
+
+        Genesis.Share memory share =
+            Genesis.Share({id: 3, owner: user, sharpEmissions: sharp, linearEmission: linear, totalCap: 120 ether});
+
+        bytes memory sig = _signShare(share, masterKey);
+
+        vm.prank(master);
+        genesis.reveal(share, sig);
+
+        vm.prank(user);
+        genesis.trigger(3);
+
+        // Total claimed should not exceed totalCap
+        assertEq(genesis.shareClaimed(3), 120 ether);
+        assertEq(token.balanceOf(user), 120 ether);
+    }
+
+    function testCannotRevealTwice() public {
+        Genesis.SharpEmission[] memory sharp;
+        Genesis.LinearEmission memory linear = Genesis.LinearEmission(0, 0, 0);
+
+        Genesis.Share memory share =
+            Genesis.Share({id: 4, owner: user, sharpEmissions: sharp, linearEmission: linear, totalCap: 0});
+
+        bytes memory sig = _signShare(share, masterKey);
+
+        vm.prank(master);
+        genesis.reveal(share, sig);
+
+        // Reveal again should revert
+        vm.prank(master);
+        vm.expectRevert("Share already revealed!");
+        genesis.reveal(share, sig);
+    }
+
+    function testCannotClaimBeforeEmission() public {
+        Genesis.SharpEmission[] memory sharp = new Genesis.SharpEmission[](1);
+        sharp[0] = Genesis.SharpEmission(block.timestamp + 1000, 100 ether);
+
+        Genesis.LinearEmission memory linear = Genesis.LinearEmission(block.timestamp + 1000, 1 ether, 10 ether);
+
+        Genesis.Share memory share =
+            Genesis.Share({id: 5, owner: user, sharpEmissions: sharp, linearEmission: linear, totalCap: 110 ether});
+
+        bytes memory sig = _signShare(share, masterKey);
+
+        vm.prank(master);
+        genesis.reveal(share, sig);
+
+        // Cannot claim yet
+        vm.prank(user);
+        vm.expectRevert("Nothing to claim!");
+        genesis.trigger(5);
     }
 }
